69ab96ae11e7baf19602d4a6b02b4d72ee07031a
Fedor Alekseev <f.alexeeff2010@yandex.ru>
2024-02-06 02:04:16 +0300

    test: add mock object ID tests

---
+++ frontend/tests/dataGateway/mockGateway/mockObjectID.test.ts
@@ -0,0 +1,15 @@
+import { findMaxMockObjectID } from "../../../src/dataGateway/mockGateway/mockObjectID";
+import { IDArrayMockMaxID, getIDArrayMock } from "./mocks/mockObjectIDMocks";
+
+describe("Mock data gateway max mock object ID in array find helper", () => {
+  test("Find result in empty array should return -1", () => {
+    const result = findMaxMockObjectID([]);
+    expect(result).toEqual(-1);
+  });
+
+  test("Find result in array should be correct", () => {
+    const IDs = getIDArrayMock();
+    const result = findMaxMockObjectID(IDs);
+    expect(result).toEqual(IDArrayMockMaxID);
+  });
+});
---
+++ frontend/tests/dataGateway/mockGateway/mocks/mockObjectIDMocks.ts
@@ -0,0 +1,17 @@
+import ID from "../../../../../common/interfaces/ID";
+
+export function getIDArrayMock(): ID[] {
+  return [
+    {
+      id: 0,
+    },
+    {
+      id: 1,
+    },
+    {
+      id: 2,
+    },
+  ];
+}
+
+export const IDArrayMockMaxID = 2;
